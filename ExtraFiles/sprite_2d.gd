4f6b6a42d9117232dc74b6f96d3edd391d72dd94
ZoomingLightningbugGirl <sophiepasquarella@gmail.com>
2026-01-15 05:31:02 +0200

    New Tilemaps & Sprite Code

--- ExtraFiles/sprite_2d.gd
+++ ExtraFiles/sprite_2d.gd
@@ -1,16 +1,18 @@
-extends Sprite2D
+extends Character
+
 func _ready():
 	print("hello world")
 func _process(_delta):
-		if Input.is_action_pressed("ClockwiseTurn"):
-			rotate(0.025)
-		if Input.is_action_pressed("CounterclockwiseTurn"):
-			rotate(-0.025)
-		if Input.is_action_pressed("MoveUp"):
-			move_local_y(-10)
-		if Input.is_action_pressed("MoveDown"):
-			move_local_y(10)
-		if Input.is_action_pressed("MoveLeft"):
-			move_local_x(-10)
-		if Input.is_action_pressed("MoveRight"):
-			move_local_x(10)
+	if Input.is_action_pressed("ClockwiseTurn"):
+		rotate(0.5)
+	if Input.is_action_just_pressed("CounterclockwiseTurn"):
+		rotate(-0.5)
+	if Input.is_action_pressed("move_right"):
+		move_local_x(50)
+	if Input.is_action_pressed("move_left"):
+		move_local_x(-50)
+	if Input.is_action_pressed("move_down"):
+		move_local_y(-50)
+	if Input.is_action_pressed("move_up"):
+		move_local_y(50)
+		

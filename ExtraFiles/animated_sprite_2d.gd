10b19ae42cb8c0c683b83c4d54892eaf4e5fc2b0
Firefly-Gardens <sojopack@gmail.com>
2026-01-27 12:33:46 -0500

    Movement controls
    Made them smoother

--- ExtraFiles/animated_sprite_2d.gd
+++ ExtraFiles/animated_sprite_2d.gd
@@ -1,14 +1,18 @@
 extends AnimatedSprite2D
 
 func movementctrls():
-	if Input.is_action_just_pressed("move_down"):
-		move_local_y(25)
-	if Input.is_action_just_pressed("move_up"):
-		move_local_y(-25)
-	if Input.is_action_just_pressed("move_right"):
-		move_local_x(25)
-	if Input.is_action_just_pressed("move_left"):
-		move_local_x(-25)
+	if Input.is_action_pressed("move_down"):
+		move_local_y(10)
+	if Input.is_action_pressed("move_up"):
+		move_local_y(-10)
+	if Input.is_action_pressed("move_right"):
+		move_local_x(10)
+	if Input.is_action_pressed("move_left"):
+		move_local_x(-10)
+	if Input.is_action_pressed("ClockwiseTurn"):
+		rotate(0.05)
+	if Input.is_action_pressed("CounterclockwiseTurn"):
+		rotate(-0.05)
 		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
